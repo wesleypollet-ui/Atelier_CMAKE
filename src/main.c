3e67893f09836aa56564a2f14bf88668ec584e07
wesleypollet-ui <wesley.pollet@2028.icam.fr>
2026-04-10 07:14:19 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,8 +28,17 @@ int main(int argc, char *argv[]){
 	}
 	else {printf("Erreur de parametres");}
 	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
-
-		
+	else if (argc == 3) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+        
+        if (strcmp(op, "car") == 0) {
+            r = _car(atof(a));
+            printf("%lf\n", r);
+        } else {
+            printf("Erreur de parametres");
+        }
+    }
+    else {printf("Erreur de parametres");}
+	
